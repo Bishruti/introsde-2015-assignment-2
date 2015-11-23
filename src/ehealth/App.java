8f504fcaa2d84b290af368712171e79c9325ea19
Bishruti <bishrutisiku7@gmail.com>
2015-11-23 18:31:34 +0100

    Add Exception Handler to debug error.

--- src/ehealth/App.java
+++ src/ehealth/App.java
@@ -7,27 +7,32 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.ParseException;
 
 public class App
 {
-    public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException
+    public static void main(String[] args)
     {
-        String protocol = "http://";
-        String port_value = "3100";
-        if (String.valueOf(System.getenv("PORT")) != "null"){
-            port_value=String.valueOf(System.getenv("PORT"));
-        }
-        String port = ":"+port_value+"/";
-        String hostname = InetAddress.getLocalHost().getHostAddress();
-        if (hostname.equals("127.0.0.1"))
-        {
-            hostname = "localhost";
-        }
-        URI BASE_URI = new URI(protocol + hostname + "ehealth/");
+        try {
+            String protocol = "http://";
+            String port_value = "3100";
+            if (String.valueOf(System.getenv("PORT")) != "null") {
+                port_value = String.valueOf(System.getenv("PORT"));
+            }
+            String port = ":" + port_value + "/";
+            String hostname = InetAddress.getLocalHost().getHostAddress();
+            if (hostname.equals("127.0.0.1")) {
+                hostname = "localhost";
+            }
+            URI BASE_URI = new URI(protocol + hostname + "ehealth/");
 
-        System.out.println("Starting sdelab standalone HTTP server...");
-        JdkHttpServerFactory.createHttpServer(BASE_URI, createApp());
-        System.out.println("Server started on " + BASE_URI + "\n[kill the process to exit]");
+            System.out.println("Starting sdelab standalone HTTP server...");
+            JdkHttpServerFactory.createHttpServer(BASE_URI, createApp());
+            System.out.println("Server started on " + BASE_URI + "\n[kill the process to exit]");
+        }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     public static ResourceConfig createApp() {
